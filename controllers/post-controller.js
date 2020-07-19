e5abfcf5967aa6e83c33688a36c40f22a0a945de
utchak160 <chakravartyutkarsh@gmail.com>
2020-07-20 00:53:25 +0530

    get and delete post added

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -26,4 +26,64 @@ const addPost = async (req, res, next) => {
     }
 };
 
-module.exports = {addPost};
+const getPosts = async (req, res, next) => {
+    try {
+        const posts = await Post.find().sort({date: -1});
+        res.json(posts);
+    } catch (e) {
+        console.log(e.message);
+        res.status(500).send({
+            msg: 'Server error'
+        });
+    }
+};
+
+const getPostById = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        if (!post) {
+            return res.status(404).send({
+                msg: 'No Post not found'
+            });
+        }
+        res.json(post);
+    } catch (e) {
+        console.log(e.message);
+        if (e.kind === 'ObjectId') {
+            return res.status(404).send({
+                msg: 'No Post not found'
+            });
+        }
+        res.status(500).send({
+            msg: 'Server error'
+        });
+    }
+};
+
+const deletePostById = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        if (!post) {
+            return res.status(404).json({msg: 'Post not found'});
+        }
+        if (post.user.toString() !== req.authData.id) {
+            return res.status(401).json({
+                msg: 'User not authorized'
+            });
+        }
+        await post.remove();
+        res.json({msg: 'Post removed'});
+    } catch (e) {
+        console.log(e.message);
+        if (e.kind === 'ObjectId') {
+            return res.status(404).send({
+                msg: 'No Post not found'
+            });
+        }
+        res.status(500).send({
+            msg: 'Server error'
+        });
+    }
+}
+
+module.exports = {addPost,getPosts, getPostById, deletePostById};
--- routes/post.js
+++ routes/post.js
@@ -8,8 +8,15 @@ const router = express.Router();
 //Add Post
 router.post('/add', [auth, [
     check('text', 'Text is required').not().isEmpty()
-]], postController);
+]], postController.addPost);
 
+//Get All posts
+router.get('/', auth, postController.getPosts);
 
+//Get Post by ID
+router.get('/:postId', auth, postController.getPostById);
+
+//Delete post by ID
+router.delete('/:postId', auth, postController.deletePostById);
 
 module.exports = router;
